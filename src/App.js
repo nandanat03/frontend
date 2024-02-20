1609f14d3fc2d26f5a282a88d5e56c761bf80c43
Nandana T <nandanasethu03@gmail.com>
2024-02-20 10:54:59 +0530

    share code on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="grid-container">
+            <header class="row">
+                <div>
+                    <a href="index.html" class="brand">amazonae</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Signin</a>
+                </div>
+            </header>
+            <main>
+                <div>
+                <div class="row center">
+                    {data.products.map((product) => (
+                        <div key={product._id} class="card">
+                        <a href={`/product/${product._id}`}>
+                            
+                            <img class="medium" src={product.image} alt={product.name}></img>
+                        </a>
+                        <div class="card-body ">
+                            <div class="row">
+                            <a href={`/product/${product._id}`}><h3>{product.name}</h3></a>
+                            <div class="rating ">
+                                <span>
+                                    <i class="fa-solid fa-heart"></i>
+                                </span>
+                            </div>
+                        </div>
+                            <div class="price">
+                                ${product.price}
+                            </div>
+                        </div>
+                     </div>
+                 
+                    ))
+                    }
+                  </div>  
+               </div>
+            </main>
+            <footer class="row center">
+                All right reserved
+            </footer>
+        </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name: 'Ray-ban ',
+            category: 'glass',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'ray-ban',
+            numlikes:20,
+            description: 'High quality'
+        },
+        {
+            _id:'2',
+            name: 'Parada',
+            category: 'glass',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Parada',
+            numlikes:34,
+            description: 'High quality'
+        },
+        {
+            _id:'3',
+            name: 'Oakley',
+            category: 'glass',
+            image: '/images/p3.jpg',
+            price: 220,
+            brand: 'Oakley',
+            numlikes:50,
+            description: 'High quality'
+        },
+        {
+            _id:'4',
+            name: 'Burrbery',
+            category: 'glass',
+            image: '/images/p4.jpg',
+            price: 150,
+            brand: 'Burrbery',
+            numlikes:12,
+            description: 'High quality'
+        },
+    ],
+};
+export default data;
